71c2a04b289b6e1359c63a0d79d9e403100c3a55
Tom Salata <salatat@oregonstate.edu>
2017-03-09 20:19:24 -0800

    added a view refugees page

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/auth', auth);
 app.use('/debugging', debugging);
-
+app.use('/view_refugees', view_refugees);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
