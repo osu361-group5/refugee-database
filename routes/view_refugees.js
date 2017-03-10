b042b33d66419eab48449fa968696d25c202e8bd
Tom Salata <salatat@oregonstate.edu>
2017-03-09 22:29:37 -0800

    trying to fix UnhandledPromiseRejectionWarning coming from view_refugees.js

--- routes/view_refugees.js
+++ routes/view_refugees.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var refugees = require('../db/queries').RefugeesDAO;
+var refugees = require('../db/queries').refugees;
 
-refugees.getRefugeesByAssociatedNGO('ngo_guy').then(function(data){
-	router.get('/', function(req, res, next) {
-		res.render('view_refugees', { title: 'NGO: Associated Refugees ', data });
-	});
+refugees.getRefugeesByAssociationWithNGO('markus')
+.then(data => {
+	console.log(data);
+})
+.catch(error => {
+	console.log(error);
 });
+
+router.get('/', function(req, res, next) {
+	res.render('view_refugees', { title: 'NGO: Associated Refugees ' });
+});
+
 
 module.exports = router;
