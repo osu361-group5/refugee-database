84fbcfbb3c9d0a37ee9304ffb088c58a5eedcd7e
Tom Salata <salatat@oregonstate.edu>
2017-03-10 22:02:54 -0800

    more changes, no results

--- routes/view_refugees.js
+++ routes/view_refugees.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db');
-var refugees = require('queries')(db).refugees;
+//var db = require('../db');
+var refugees = require('../db/queries').refugees;
 
-router.get('/', function(req, res) {
-	refugees.getRefugeesByAssociationWithNGO('markus').then((data)=>res.render('view_refugees', results=data));
+router.get('/view_refugees', function(req, res) {
+	refugees.getRefugeesByAssociationWithNGO('markus').then((data)=>res.render('view_refugees', data));
 });
+/*router.get('/view_refugees/:username', function(req, res) {
+	refugees.getRefugeesByAssociationWithNGO(req.params.username).then((data)=>res.render('view_refugees', data));
+});*/
 
 module.exports = router;
