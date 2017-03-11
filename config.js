5b7d7c1d1a1bfac075347fe54df4cad6a3571285
Tom Salata <salatat@oregonstate.edu>
2017-03-11 11:25:22 -0800

    view_refugees functioning, but returns error as table user_m doesn't exist

--- config.js
+++ config.js
@@ -5,11 +5,3 @@ module.exports = {
     dbPass: process.env.dbPass || 'postgres',
     port: process.env.PORT | 3000
 };
-/*
-module.exports = {
-    dbHost: process.env.dbHost || 'ec2-23-23-237-68.compute-1.amazonaws.com',
-    dbDatabase: process.env.dbDatabase || 'd3108blvvmu40a',
-    dbUser: process.env.dbUser || 'cplhcefkzookat',
-    dbPass: process.env.dbPass || '2d8215d6d542c7a90314e0c36ea78b045f4f2167bf911393021f3a183828017f',
-    port: process.env.PORT | 5432
-};*/
--- routes/view_refugees.js
+++ routes/view_refugees.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
-//var db = require('../db');
-var refugees = require('../db/queries').refugees;
+var db = require('../db');
+var refugees = require('../db/queries')(db).refugees;
 
-router.get('/view_refugees', function(req, res) {
-	refugees.getRefugeesByAssociationWithNGO('markus').then((data)=>res.render('view_refugees', data));
+router.get('/', function(req, res) {
+	refugees.getRefugeesByAssociationWithNGO('markus')
+		.then((data)=> {
+			res.render('view_refugees', context=data);
+			done();
+		})
+		.catch((err) => {
+			console.log('ERROR: ',err);
+			res.render('view_refugees', null);
+			done();
+		});
 });
-/*router.get('/view_refugees/:username', function(req, res) {
-	refugees.getRefugeesByAssociationWithNGO(req.params.username).then((data)=>res.render('view_refugees', data));
-});*/
 
 module.exports = router;
