62a7fa94cd2566fed825f0bc186c8838dd86d366
Tom Salata <salatat@oregonstate.edu>
2017-03-10 21:29:50 -0800

    changed to reflect new DAO instance creation

--- routes/view_refugees.js
+++ routes/view_refugees.js
@@ -1,21 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var refugees = require('../db/queries').refugees;
+var db = require('../db');
+var refugees = require('queries')(db).refugees;
 
-refugees.getRefugeesByAssociationWithNGO('markus')
-.then(data => {
-	console.log(data);
-})
-.catch(error => {
-	console.log(error);
+router.get('/', function(req, res) {
+	refugees.getRefugeesByAssociationWithNGO('markus').then((data)=>res.render('view_refugees', results=data));
 });
 /*router.get('/view_refugees/:username', function(req, res) {
 	refugees.getRefugeesByAssociationWithNGO(req.params.username).then((data)=>res.render('view_refugees', data));
 });*/
-
-router.get('/', function(req, res, next) {
-	res.render('view_refugees', { title: 'NGO: Associated Refugees ' });
-});
-
 
 module.exports = router;
