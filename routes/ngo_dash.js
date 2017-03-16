a283236785ca78aac9e89512d6a4beb12e1b1f84
Tom Salata <salatat@oregonstate.edu>
2017-03-15 18:23:25 -0700

    fixed button style and error in link path for ngo dash

--- routes/ngo_dash.js
+++ routes/ngo_dash.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('ngo_options', { title: 'NGO Options', user: req.session.username });
+	res.render('ngo_dash', { title: 'NGO Dashboard', user: req.session.username });
 });
 
 module.exports = router;
