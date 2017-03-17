0a7005a0746a6cbd2764e314020a18da20188c1a
sohailkhanosu <khanso@oregonstate.edu>
2017-03-16 21:49:06 -0700

    add debugging set session routes

--- routes/debugging.js
+++ routes/debugging.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var setupDb = require('../db/util');
 var db = require('../db');
+var queries = require('../db/queries')(db);
 
 var router = express.Router();
 var sqlFile = 'db/schema.sql';
@@ -21,4 +22,31 @@ router.get('/reset-db', (req, res, next) => {
         .catch((err) => next(new Error(err)))
 });
 
+router.get('/set-refugee-session', (req, res, next) => {
+    if (req.session.username)  {
+        req.session.destroy();
+        res.redirect('/debugging/set-refugee-session');     // recreate session
+    }
+    req.session.username = queries.testData.testUser.username;
+    req.session.userId = queries.testData.testUser.id;
+    req.session.isLoggedIn = true;
+    res.redirect('/refugee_dashboard');
+});
+
+
+router.get('/set-ngo-session', (req, res, next) => {
+    if (req.session.username)  {
+        req.session.destroy();
+        res.redirect('/debugging/set-ngo-session');     // recreate session
+    }
+    req.session.username = queries.testData.testNGOUser.username;
+    req.session.userId = queries.testData.testNGOUser.id;
+    req.session.isLoggedIn = true;
+    res.redirect('/ngo_dash');
+});
+
+router.get('/destroy-session', (req, res, next) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 module.exports = router;
