29c1cf80698256d2e69eb78370764d4c75c8582d
lt <tynerl@oregonstate.edu>
2017-03-18 11:20:09 -0500

    User Story 3.a complete in most basic form

--- routes/refugee_dashboard.js
+++ routes/refugee_dashboard.js
@@ -4,10 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var passedVariable = req.query.status;
-    console.log(req.query);
     if (passedVariable == 1) {
       var result = "Successfully added Associated Person";
+    } else if (passedVariable == 0) {
+      var result = "Functionality not yet implemented.";
     }
+
   res.render('refugee_dashboard', { title: 'Refugee Dashboard', status: result});
 });
 
