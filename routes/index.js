4947d393d0cea0f3bd509481c7faa5e7ba8f22d0
sohailkhanosu <khanso@oregonstate.edu>
2017-03-19 00:05:04 -0700

    add registration handlers/tests and fix registration form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
+var {users, refugees, ngo} = require('../db/queries')(db);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Refugee Database' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.post('/register', function(req, res, next) {
+  var {username, password, email,name,organization} = req.body;
+  var userType = req.body['user-type'];
+
+    if (!(username && password && email && userType)) {
+        return res.render("register", {"error": "Please make sure you have completed the form correctly."});
+    }
+
+  users.createUser(username, password, email)
+      .then((data) => {
+          if (userType === "refugee") {
+              return refugees.create(data.id,name);
+          } else {
+              return ngo.create(data.id, organization);
+          }
+      })
+      .then(() => {
+          res.redirect("/auth/login");
+      })
+      .catch((err) => {
+          var error = new Error('not able to fulfill the request');
+          return next(error);
+      });
 });
 
 module.exports = router;
